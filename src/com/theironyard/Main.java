08bddf981c6d17357b028b0d284c9c5064ef28f7
willriggins <williamriggins2013@gmail.com>
2016-06-01 09:29:18 -0400

    anonymous class

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,17 +3,41 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-        Dog d = new Dog();
-        Snake s = new Snake();
-        Hawk h = new Hawk();
+        Animal d = createAnimal("Dog");
+        Animal s = createAnimal("Snake");
+        Animal h = createAnimal("Hawk");
+
+        Animal a = new Reptile() {
+            @Override // this is purely just to catch an error.
+            public void makeSound() {
+                System.out.println("Croak!");
+            }
+        };
+        a.name = "Alligator";
 
 
         d.makeSound();
         s.makeSound();
         h.makeSound();
+        a.makeSound();
 
         System.out.println(d);
         System.out.println(s);
         System.out.println(h);
+    }
+
+    //may help with assignment - new factory method below:
+    public static Animal createAnimal(String name) {
+        switch (name) {
+            case "Dog":
+                return new Dog();
+
+            case "Snake":
+                return new Snake();
+
+            case "Hawk":
+                return new Hawk();
+        }
+        return new Animal();
     }
 }
